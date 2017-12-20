916c1b642be824a18d806bf4c008a4182788f970
piyo56 <piyo56.net@gmail.com>
2017-12-20 23:47:51 +0900

    update preprocess.py to train the model using label

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,7 @@ Perform following preprocessing for ** MUG Facial Dataset **.
  2. extract subsequences of video clips
  3. crop the face region
  4. resize 64 x 64
+ 5. save samples for each category
 
 To use this script, pass the arguments
 MUG dataset directory and save directory.
@@ -22,17 +23,30 @@ in MUG dataset
      |--- 002
            :
 """
-
+import argparse
 import sys, os, glob, shutil
 import cv2
 import re
 
-FRAME_NUMBER = re.compile(r'img_([0-9]+).jpg')
 CASCADE_PATH = "data/cv_models/haarcascade_frontalface_default.xml"
 
-def fname2num(name):
-    match = re.search(FRAME_NUMBER, name)
+frame_name_regex = re.compile(r'([0-9]+).jpg')
+
+def frame_number(name):
+    match = re.search(frame_name_regex, name)
     return match.group(1)
+
+video_info_regex = re.compile(r'.*?\/(?P<user_num>[0-9]+)\/(session(?P<session_num>[0-9])\/)?(?P<expression>.*?)\/take(?P<take_num>[0-9]+)')
+
+def extract_video_info(name):
+    match = re.search(video_info_regex, name)
+    user_num    = int(match.group('user_num'))
+    session_num = match.group('session_num')
+    session_num = 0 if session_num is None else int(session_num)
+    expression  = match.group('expression')
+    take_num    = int(match.group('take_num'))
+
+    return user_num, session_num, expression, take_num
 
 def show_img(img):
     cv2.imshow('image', img)
@@ -53,77 +67,86 @@ def detect_face(image):
 
     return rect
 
-def main(dataset_path, output_path):
-    # list all video clips
-    facial_expressions = ["anger", "disgust", "happiness",
-                          "fear", "sadness", "surprise"]
-    video_paths = []
-    for exp in facial_expressions:
-        takes = glob.glob(os.path.join(dataset_path, '*', exp, '*'))
-        # good_takes = list(filter(lambda name: name.find('nsg') < 0, takes))
-        # video_paths.extend(good_takes)
-        video_paths.extend(takes)
+def main():
+    parser = argparse.ArgumentParser(description='Preprocessing script for MUG Facial Expression Database')
+    parser.add_argument('dataset_path', type=str)
+    parser.add_argument('save_path', type=str)
+    # parser.add_argument('--use_labels', action='store_true')
+    args = parser.parse_args()
 
-    print(">>> {} video clips found.".format(len(video_paths)))
-    
     edge   = 0.10 # dont use end of frames of the video
     speeds = [2] # use 1 frame per speed frames ( to change speed )
     length = 16 # video length (frame num)
     stride = length // 2 # stride width
 
-    # perform preprocess
-    saved_num = 0
-    os.makedirs(output_path, exist_ok=True)
-    for i, in_dir in enumerate(video_paths):
-        images = glob.glob(os.path.join(in_dir, '*.jpg'))
-        images = sorted(images, key=fname2num)
+    # list all video clips
+    facial_expressions = ["anger", "disgust", "happiness",
+                          "fear", "sadness", "surprise"]
+    samples_count = 0
+    summary = "" 
+    for label, exp in enumerate(facial_expressions):
+        video_paths = glob.glob(os.path.join(args.dataset_path, '*', exp, '*'))
+        # good_takes = list(filter(lambda name: name.find('nsg') < 0, takes))
+        # video_paths.extend(good_takes)
+        samples_count += len(video_paths)
+        print(">>> {}: {} video clips found.".format(exp, len(video_paths)))
+    
+        # perform preprocess
+        saved_num = 0
+        category_path = os.path.join(args.save_path, str(label))
+        os.makedirs(category_path, exist_ok=True)
+        for i, in_dir in enumerate(video_paths):
+            images = glob.glob(os.path.join(in_dir, '*.jpg'))
+            images = sorted(images, key=frame_number)
 
-        edge_frames = int(len(images) * edge)
-        start = 0
-        end   = len(images) - edge_frames
-        for speed in speeds:
-            frame_width = speed*length
-            for seq_n, j in enumerate(range(start, end-frame_width, stride)[0:-1]):
-                out_dir = os.path.join(output_path, "{:04d}_{:d}_{:02d}".format(i+1, speed, seq_n+1))
-                os.makedirs(out_dir, exist_ok=True)
-            
-                # detect face region of the video clip
-                mid_frame = j + frame_width//2
-                image = cv2.imread(images[mid_frame])
-                rect = detect_face(image)
-                if rect is None:
-                    shutil.rmtree(out_dir)
-                    continue
+            edge_frames = int(len(images) * edge)
+            start = 0
+            end   = len(images) - edge_frames
+            for speed in speeds:
+                frame_width = speed*length
+                for seq_n, j in enumerate(range(start, end-frame_width, stride)[0:-1]):
+                    user_num, session_num, expression, take_num = extract_video_info(in_dir)
+                    out_dir = os.path.join(category_path, "user{:03d}_sess{}_take{:03d}_{:02d}".format(user_num, session_num, take_num, seq_n))
+                    os.makedirs(out_dir, exist_ok=True)
                 
-                for k in range(length):
-                    # read img
-                    image = cv2.imread(images[j+speed*k])
+                    # detect face region of the video clip
+                    mid_frame = j + frame_width//2
+                    image = cv2.imread(images[mid_frame])
+                    rect = detect_face(image)
+                    if rect is None:
+                        shutil.rmtree(out_dir)
+                        continue
+                    
+                    for k in range(length):
+                        # read img
+                        image = cv2.imread(images[j+speed*k])
 
-                    # crop face part
-                    x, y = rect[0], rect[1]
-                    w, h = rect[2], rect[3]
-                    image = image[y:y+h, x:x+w]
+                        # crop face part
+                        x, y = rect[0], rect[1]
+                        w, h = rect[2], rect[3]
+                        image = image[y:y+h, x:x+w]
 
-                    # resize 64, 64
-                    image = cv2.resize(image,(64, 64))
+                        # resize 64, 64
+                        image = cv2.resize(image,(64, 64))
 
-                    # save
-                    image_name = "{:02d}.jpg".format(k+1)
-                    cv2.imwrite(os.path.join(out_dir, image_name) , image)
+                        # save
+                        image_name = "{:02d}.jpg".format(k+1)
+                        cv2.imwrite(os.path.join(out_dir, image_name) , image)
 
-                saved_num += 1
-                print("{}({} frames) --> {}(offset:{}, speed:{})".format(
-                                                        '/'.join(in_dir.split('/')[-3:]),
-                                                        len(images),
-                                                        '/'.join(out_dir.split('/')[-2:]),
-                                                        j, speed))
-        print("")
+                    saved_num += 1
+                    print("{}({} frames) --> {}(offset:{}, speed:{})".format(
+                                                            '/'.join(in_dir.split('/')[-3:]),
+                                                            len(images),
+                                                            '/'.join(out_dir.split('/')[-2:]),
+                                                            j, speed))
+            print("")
 
-    print(">>> preprocess finished and {} video clips saved".format(saved_num))
+        print(">>> preprocess finished and {} video clips saved".format(saved_num))
 
 if __name__=="__main__":
-    if len(sys.argv) < 3:
-        print("usage: python preprocess.py <dataset path> <output path>")
-        sys.exit()
-
-    main(sys.argv[1], sys.argv[2])
+    main()
+    # if len(sys.argv) < 3:
+    #     print("usage: python preprocess.py <dataset path> <output path>")
+    #     sys.exit()
+    #
+    # main(sys.argv[1], sys.argv[2])
