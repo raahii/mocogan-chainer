bda0e9aabaa507265151a0229a25d20854f7287e
piyo56 <piyo56.net@gmail.com>
2018-02-04 15:38:08 +0900

    update visualization code

--- visualize.py
+++ visualize.py
@@ -1,4 +1,4 @@
-import sys, os, glob, subprocess, shutil
+import sys, os, glob, shutil
 import re
 from multiprocessing import Process
 
@@ -10,120 +10,92 @@ import chainer.cuda
 from chainer import Variable
 import chainer.functions as F
 
+import subprocess as sp
+
+def to_sequence(video, horizontally=True):
+    """Convert a video to an image with frames horizontally aligned
+
+    :param np.ndarray video: video (dim=5, axis=(num, channel, height, width))
+    :param bool horizontally: whether concatenate horizontally or not (vertically)
+    """
+    # write video
+    N, C, H, W = video.shape
+    
+    axis = horizontally and 2 or 1
+    seq_image = video[0]
+    for i in range(1, N):
+        seq_image = np.concatenate((seq_image, video[i]), axis=axis)
+
+    return seq_image
+
 def to_grid(videos, size):
     """
-    arrange images in a (size, size) grid
+    Convert videos to a size x size grid video
 
-    videos : target videos array
-             shape:(batchsize, video_length, channel, height, width)
-    size: this function create size x size grid video
+    :param np.ndarray video: video (dim=5, axis=(video_len, batchsize, channel, height, width))
+    :param int size: size of one side of the grid
     """
 
-    bs, t, h, w, c = videos.shape
+    t, bs, c, h, w = videos.shape
     
     # make (size x size) grid
     if bs < size*size:
         bnum = size*size - bs
-        blank_videos = np.zeros((bnum, t, h, w, c))
-        videos = np.concatenate((videos, blank_videos))
+        blank_videos = np.zeros((t, bnum, c, h, w), dtype=np.uint8)
+        videos = np.concatenate((videos, blank_videos), axis=1)
 
+    grid_video = np.empty((t, c, size*h, size*w), dtype=videos.dtype)
     for i in range(size):
-        row = videos[size*i]
+        for j in range(size):
+            grid_video[:, :, i*h:i*h+h, j*w:j*w+w] = videos[:, i*size+j]
 
-        for j in range(1, size):
-            row = np.concatenate((row, videos[size*i+j]), axis=2)
+    return grid_video
 
-        if i == 0:
-            grid_videos = row
-        else:
-            grid_videos = np.concatenate((grid_videos, row), axis=1)
-    
-    return grid_videos.astype(np.uint8)
+def save_frames(video, dirname):
+    for i, v in enumerate(video):
+        filename = os.path.join(dirname, "{:02d}.jpg".format(i))
+        Image.fromarray(v).save(filename)
 
-def save_grid_video(video, save_dir, basename, sub_save_dir):
-    """
-    save video under save_dir.
+def save_video(video, filename):
+    t, h, w, c  = video.shape
+    command = ['ffmpeg',
+               '-y',
+               '-f', 'rawvideo',
+               '-vcodec', 'rawvideo',
+               '-s', '{}x{}'.format(h,w),
+               '-pix_fmt', 'rgb24',
+               '-r', '8',
+               '-i', '-',
+               '-c:v', 'mjpeg',
+               '-q:v', '3',
+               '-an',
+               filename]
+    pipe = sp.Popen(command, stdin=sp.PIPE, stderr=sp.PIPE)
+    pipe.stdin.write(video.tostring())
 
-    input
-        video: np.array, shape is (t, y, x, ch)
-        save_dir: path to save video
-        sub_save_dir: path to save intermidiate files 
-    """
-
-    t, y, x, ch = video.shape
-    
-    image_path = os.path.join(sub_save_dir, 'image')
-    os.makedirs(image_path, exist_ok=True)
-    for i in range(t):
-        image_file = os.path.join(image_path, "{:02d}.jpg".format(i))
-        Image.fromarray(video[i,:,:,0:3]).save(image_file)
-        
-    cmd = 'ffmpeg -y -r 19 -i {} -vcodec libx264 -pix_fmt yuv420p -vf setpts=PTS/0.5 {}'.format(os.path.join(image_path, "%02d.jpg"), os.path.join(save_dir, basename+".mp4"))
-    subprocess.call(cmd, shell=True)
-
-
-def save_video_samples(image_gen, num, size, ch, T, seed, writer):
-    @chainer.training.make_extension()
-    def make_video(trainer):
-        with chainer.using_config('train', False):
-            np.random.seed(seed)
-            updater = trainer.updater
-
-            save_dir = os.path.join(save_path, 'samples', 'epoch_{:04d}'.format(updater.epoch))
-            os.makedirs(save_dir, exist_ok=True)
-
-            # generate samples
-            h0 = Variable(updater.converter(image_gen.make_h0(num), updater.device))
-            videos = image_gen(h0)
-            videos = chainer.cuda.to_cpu(videos.data)
-            # (t, bs, ch, x, y)
-            videos = (videos + 1.0) / 2. * 255.
-            videos = videos.astype(np.uint8)
-            videos = videos.transpose(1, 0, 3, 4, 2)
-
-            for i in range(num): # batch
-                video_path = os.path.join(save_dir, "{:02d}".format(i+1))
-                os.makedirs(video_path, exist_ok=True)
-                write_video(videos[i], video_path)
-                # np.save(video_path, video[:, i, 0])
-
-    return make_video
-
-def log_tensorboard(image_gen, num, use_labels, seed, writer, save_path):
+def log_tensorboard(image_gen, num, video_length, writer):
     @chainer.training.make_extension()
     def log(trainer):
         with chainer.using_config('train', False):
-            np.random.seed(seed)
             updater = trainer.updater
             
             # generate samples
             h0 = Variable(updater.converter(image_gen.make_h0(num), updater.device))
-            if use_labels:
-                if num == 36:
-                    labels = np.repeat(np.arange(6), 6)
-                    videos, labels = image_gen(h0, labels=labels)
-                else:
-                    videos, labels = image_gen(h0)
-            else:
-                videos = image_gen(h0)
-            videos = chainer.cuda.to_cpu(videos.data)
-
-            videos = (videos + 1.0) / 2. * 255.
-            videos = videos.astype(np.uint8)
-            # final videos shape: (bs, t, y, x, ch)
-            videos = videos.transpose(1, 0, 3, 4, 2)
+            videos, _ = image_gen(h0)
+            videos = chainer.cuda.to_cpu(videos.data) # (T, N, C, H, W)
+            videos = videos / 2. + 0.5
             
-            # log to tensorboard
-            grid_videos = to_grid(videos, int(np.sqrt(num)))
-            grid_videos = grid_videos.transpose(0, 3, 1, 2)
-            for t in range(len(grid_videos)):
-                writer.add_image('{:02d}'.format(t), grid_videos[t,0:3] / 255., updater.epoch)
+            # make grid video, log only part of video frames.
+            grid_video = to_grid(videos, int(np.sqrt(num))) # (T, C, H, W)
             
-            # save video to directory
-            # grid_videos = grid_videos.transpose(0, 2, 3, 1)
-            # save_dir = os.path.join(save_path, 'samples')
-            # basename = 'epoch_{:04d}'.format(updater.epoch)
-            # sub_save_dir = os.path.join(save_dir, basename)
-            # os.makedirs(sub_save_dir, exist_ok=True)
-            # save_grid_video(grid_videos, save_dir, basename, sub_save_dir)
+            ## image shape: (C, H, W), value range: [0, 1.0]
+            for i in np.linspace(0, video_length, 4, endpoint=False, dtype=np.int):
+                writer.add_image('{:02d}th frame'.format(i), grid_video[i], updater.epoch)
+            
+            # write videos as image
+            for i in range(10):
+                video = videos[:, i]
+                video = to_sequence(video)
+                writer.add_image('video_{:02d}'.format(i), video, updater.epoch)
+            
     return log
